9e83c8c4143cc9ab21604dcc9ec59b5decd48e70
AlekseyFrontEndDev <kino45.1982@gmail.com>
2019-03-28 17:02:09 +0200

    fixes

--- source/js/modules/header-menu-open.js
+++ source/js/modules/header-menu-open.js
@@ -1,5 +1,6 @@
 (function () {
   var gamburger = document.querySelector('.header__toggle-btn');
+  // var gamburger = document.querySelector('.new-header__toggle-btn');
   // header__nav
   var navigarion = document.querySelector('.header__nav');
 
