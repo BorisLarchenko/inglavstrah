dcc4254bf5de6bd2be20a412324df46938cf9b34
Boris Larchenko <borislarchenko@gmail.com>
2019-04-26 17:28:30 +0300

    fix js issue while minification add oportunity to us ES6

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -1,6 +1,24 @@
 (function () {
+  //find form
+  const form = document.querySelector('.test__form');
+  console.log(form);
+//  find button-forward
+  const buttonForwardOne = form.querySelector('.test__form-step1 .test__btn');
+  //find current div
+  const formStepOne = form.querySelector('.test__form-step1');
+  console.log(formStepOne);
+  //find next div
+  const formStepTwo = form.querySelector('.test__form-step1 + div');
+  console.log(formStepTwo);
 
-  console.log('hello');
+  const formStepNth = form.querySelector('.test__form:nth-child(4)');
+  console.log(formStepNth);
+
+  buttonForwardOne.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    formStepOne.classList.remove('test__form-step1');
+    formStepTwo.classList.add('test__form-step2');
+  })
 
 })();
 
