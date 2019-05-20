1df2665f609a14e538f2911e947d40125db98948
Boris Larchenko <borislarchenko@gmail.com>
2019-05-20 14:48:18 +0300

    add test/delete .review-thanks-open button for contacts.html

--- source/js/modules/contacts.js
+++ source/js/modules/contacts.js
@@ -1,10 +1,25 @@
 (function () {
-  var shopsTown = document.querySelector('.shops__town');
+  const shopsTown = document.querySelector('.shops__town');
 
 
   shopsTown.addEventListener('click', function() {
     shopsTown.classList.toggle('shops__town--open');
   });
+
+
+  //find elements
+  const btnTest = document.querySelector('.review-thanks-open');
+  const blocksArr = document.querySelectorAll('.form__contacts');
+  //open/close blocksArr while btnTest is clicked
+  if (blocksArr.length !== 0) {
+    btnTest.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      for (let i =0; i < blocksArr.length; i++) {
+        blocksArr[i].classList.toggle('form__contacts--close');
+      }
+    })
+  }
+
 })();
 
 $(document).ready(function(){
