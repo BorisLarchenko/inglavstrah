8d37a3bb5b77d536adbf34ca5dde857e8ceb60bf
AlekseyFrontEndDev <kino45.1982@gmail.com>
2019-06-12 17:10:42 +0300

    add semantic library for calendar and dropdowns

--- source/js/modules/kbm-steps.js
+++ source/js/modules/kbm-steps.js
@@ -1,7 +1,18 @@
 (function () {
   function testKbmSteps(elem){
-    console.log('hello kbm-steps!');
-    console.log(elem);
+    $('.calendar-label-date').calendar({
+      type: 'date',
+      startMode: 'year',
+      monthFirst: false,
+      text: {
+          days: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+          months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Окрябрь', 'Ноябрь', 'Декабрь'],
+          monthsShort: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+          today: 'Сегодня',
+          now: 'Сейчас'
+        },
+    });
+    $('.ui.dropdown').dropdown();
     //find full form in full-request page
     const form = elem.querySelector('.test__form');
 
