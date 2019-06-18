193f17b3321598f082240a2130d8c56d4d96ac4d
Boris Larchenko <borislarchenko@gmail.com>
2019-06-18 16:56:27 +0300

    make adaptive .main__advantages section in index.html

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -6,4 +6,23 @@ $(document).ready(function(){
     slidesToShow: 1,
     adaptiveHeight: true
   });
+
+  $('.main__advantages-list--slider').slick({
+    slide: '.main__advantages-item--slide',
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: false,
+    nextArrow: false,
+
+    mobileFirst: true,
+    // centerMode: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: "unslick"
+      }
+    ]
+  });
 });
