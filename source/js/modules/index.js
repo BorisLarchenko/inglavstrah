eaf38c464d86e1652a34ddd0ea0f08c70919ec6d
Boris Larchenko <borislarchenko@gmail.com>
2019-06-19 15:44:46 +0300

    add adaptive for tablet for main__howto-slider index.html

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -4,7 +4,13 @@ $(document).ready(function(){
     infinite: false,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1280,
+        arrows: false,
+      }
+    ]
   });
 
   $('.main__advantages-list--slider').slick({
@@ -13,8 +19,7 @@ $(document).ready(function(){
     infinite: false,
     speed: 300,
     slidesToShow: 1,
-    prevArrow: false,
-    nextArrow: false,
+
 
     mobileFirst: true,
     // centerMode: true,
