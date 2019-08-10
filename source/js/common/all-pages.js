be89537ae61f540df0c563c276ccd351bc4bd8b8
AlekseyFrontEndDev <kino45.1982@gmail.com>
2019-08-10 19:18:40 +0300

    прелоадер

--- source/js/common/all-pages.js
+++ source/js/common/all-pages.js
@@ -39,3 +39,13 @@ $(function(){
       };
     }
   });
+
+
+// START Hide preloader after load page
+  $(window).on('load', function () {
+      var $preloader = $('#page-preloader'),
+          $spinner   = $preloader.find('.spinner');
+      $spinner.fadeOut();
+      $preloader.delay(350).fadeOut('slow');
+  });
+// FINISH Hide preloader after load page
