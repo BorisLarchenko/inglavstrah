7a622104f5f6dd21bc490e99a834d44a93222bfd
AlekseyFrontEndDev <kino45.1982@gmail.com>
2019-10-06 16:28:42 +0300

    new Insurance page

---
+++ source/js/modules/insurance.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('.insur__details-trigger').click(function() {
+		if( $( this ).hasClass('open') ){
+			$( this ).removeClass('open');
+		} else{
+			$('.insur__details-trigger').removeClass('open');
+			$( this ).addClass('open');
+		}
+	});
+
+	$('.open_hidden_content').click(function() {
+		let that = this,
+				attr = $( this ).attr('id');
+
+		if( $( this ).hasClass('default') ){
+			that.innerHTML = "Свернуть";
+			$( this ).removeClass('default');
+
+			$('.hidden-content[data-company=' + attr + ']').removeClass('hidden');
+		} else{
+			that.innerHTML = "Смотреть всю информацию";
+			$( this ).addClass('default');
+
+			$('.hidden-content[data-company=' + attr + ']').addClass('hidden');
+		};
+	});
+
+});
